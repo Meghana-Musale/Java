85d635aa626f1c685fb4cc938cd5e8193f4af059
Meghana-Musale <musalemeghana3@gmail.com>
2025-08-02 20:13:07 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,10 @@
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello";
+		StringBuilder reversed = new StringBuilder(str).reverse();
+		System.out.println(reversed);
+}
+}
