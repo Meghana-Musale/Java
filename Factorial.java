58fb47be2c2efe5d17bfccb0860a15a57b44a707
Meghana-Musale <musalemeghana3@gmail.com>
2025-08-03 21:00:09 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int factorial = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		for(int i = 1; i <= num; i++) {
+			factorial = factorial * i;
+		}
+		System.out.println(factorial);
+	}
+
+}
