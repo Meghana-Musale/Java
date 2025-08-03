a286db5371154d26218c6332fbbca413cf7ef811
Meghana-Musale <musalemeghana3@gmail.com>
2025-08-03 21:06:54 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrimeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int num = sc.nextInt();
+		boolean isPrime = true;
+		for(int i = 2; i<= Math.sqrt(num); i++) {
+			if(num % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		System.out.println(isPrime);
+	}
+
+}
