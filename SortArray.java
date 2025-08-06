efc674af9df8a87b85ca006459ebb0db6b39892a
Meghana-Musale <musalemeghana3@gmail.com>
2025-08-06 22:43:04 +0530

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class SortArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {4,2,1,5,3};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+}
